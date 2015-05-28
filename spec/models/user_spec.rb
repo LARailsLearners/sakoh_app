43ad87fd8e7aba89c6f0df5db02567ee6924527e
sakoh <sakohartounian@yahoo.com>
2015-05-27 23:03:50 -0700

    first name and last name now works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "create new user" do
+
+    	user = User.new(
+    		id: 1,
+    		first_name: "Sako",
+    		last_name: "Hartounian", 
+    		email: "sakohartounian@yahoo.com", 
+    		password: "12345", 
+    		password_confirmation: "12345"
+    	)
+
+    	user.save
+
+    	expect(User.find_by(email: "sakohartounian@yahoo.com")).to all(user)
+
+	end
+
 end
