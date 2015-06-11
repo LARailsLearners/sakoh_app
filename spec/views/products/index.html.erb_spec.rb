927295780beb3477087e7080e04b1139291479b4
sakoh <sakohartounian@yahoo.com>
2015-06-11 10:31:28 -0700

    conditional added to display of production on the product index page depending on whether it is greater or lesser than 120

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -6,22 +6,26 @@ RSpec.describe "products/index", type: :view do
   before(:each) do
     assign(:products, [
       Product.create!(
-        :name => "Name",
+        :name => "Name 1",
         :description => description,
-        :price => 1.5
+        :price => 1.50
       ),
       Product.create!(
-        :name => "Name",
-        :description => description,
-        :price => 1.5
+        :name => "Name 2",
+        :description => "Hello World",
+        :price => 2.50
       )
     ])
   end
 
   it "renders a list of products" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "#{description.slice(0,120)} ...".to_s, :count => 2
-    assert_select "tr>td", :text => 1.5.to_s, :count => 2
+    assert_select "tr>td", :text => "Name 1".to_s
+    assert_select "tr>td", :text => "#{description.slice(0,120)} ...".to_s
+    assert_select "tr>td", :text => 1.50.to_s
+
+    assert_select "tr>td", :text => "Name 2".to_s
+    assert_select "tr>td", :text => "Hello World".to_s
+    assert_select "tr>td", :text => 2.50.to_s
   end
 end
