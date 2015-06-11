6066ec1c3c7dab7b9f33a9332d12dc66846dee4c
sakoh <sakohartounian@yahoo.com>
2015-06-10 20:48:32 -0700

    Changes to be committed: 	modified:   db/migrate/20150521163652_devise_create_users.rb 	new file:   db/migrate/20150611032931_add_first_name_last_name_to_users.rb 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604191621) do
+ActiveRecord::Schema.define(version: 20150611032931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 20150604191621) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -39,6 +37,8 @@ ActiveRecord::Schema.define(version: 20150604191621) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
