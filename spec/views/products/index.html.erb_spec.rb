86c0f95003b1afcff967456d468d51b139bb64cd
sakoh <sakohartounian@yahoo.com>
2015-06-10 18:46:56 -0700

    short description added for product index page

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum elementum varius sollicitudin. Nunc id libero vel orci ornare gravida. Quisque dapibus augue eget nunc ornare, sed malesuada lorem gravida. Donec non porttitor turpis laoreet. Ut faucibus tempus ultrices. Cras in laoreet dui. In hac habitasse platea dictumst."
+
 RSpec.describe "products/index", type: :view do
   before(:each) do
     assign(:products, [
       Product.create!(
         :name => "Name",
-        :description => "MyText",
+        :description => description,
         :price => 1.5
       ),
       Product.create!(
         :name => "Name",
-        :description => "MyText",
+        :description => description,
         :price => 1.5
       )
     ])
@@ -19,7 +21,7 @@ RSpec.describe "products/index", type: :view do
   it "renders a list of products" do
     render
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "#{description.slice(0,120)} ...".to_s, :count => 2
     assert_select "tr>td", :text => 1.5.to_s, :count => 2
   end
 end
