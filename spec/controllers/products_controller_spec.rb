f0ef21f71781a5469fd2d204eeff76b52d26ceb5
sakoh <sakohartounian@yahoo.com>
2015-06-19 16:59:26 -0700

    new tests added to the ProductsController to cover previously written code

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -24,9 +24,6 @@ RSpec.describe ProductsController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Product. As you add validations to Product, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
 
   let(:invalid_attributes) {
     skip("Add a hash of attributes invalid for your model")
@@ -34,22 +31,25 @@ RSpec.describe ProductsController, type: :controller do
 
   let(:user) { create(:user) }
 
+  let(:different_user) { User.create(first_name: "John", last_name: "Smith", email: "jsmith@gmail.com") }
+
   let(:product_attrs) { attributes_for(:product) }
 
-  let(:product) do
-    user.products.create(attributes_for(:product))
-  end
+  let(:product) { user.products.create(attributes_for(:product)) }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ProductsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_attributes) {
+    attributes_for(:product)
+  }
+
+  let(:invalid_attributes) do
+    create(:product)
+  end
 
   describe "GET #index" do
 
     it "tries to assign the requested product as @products" do
       sign_in nil
-      get :index, {}, valid_session
+      get :index, {}
       expect(response).to be_success
       expect(assigns(:products)).to eq([product])
     end
@@ -59,7 +59,7 @@ RSpec.describe ProductsController, type: :controller do
   describe "GET #show" do
 
     it "tries to assign the requested product as @product" do
-      get :show, {:id => product.to_param}, valid_session
+      get :show, {:id => product.to_param}
       expect(assigns(:product)).to eq(product)
     end
 
@@ -69,115 +69,122 @@ RSpec.describe ProductsController, type: :controller do
 
     it "tries to assign a new product as @product without authorization" do
       sign_in nil
-      get :new, {}, valid_session
+      get :new, {}
       expect(response).to redirect_to(new_user_session_path)
     end
 
     it "assigns a new product as @product with authorization" do
       sign_in user
-      get :new, {}, valid_session
+      get :new, {}
       expect(assigns(:product)).to be_a_new(Product)
     end
   end
 
   describe "GET #edit" do
 
-    it "tries to assign the requested product as @product without authorization" do
+    it "tries to get the requested product edit page without logging in" do
       sign_in nil
-      get :edit, {:id => product.to_param}, valid_session
+      get :edit, {:id => product.to_param}
       expect(response).to redirect_to(new_user_session_path)
     end
 
-    it "assigns the requested product as @product with authorization" do
+    it "tries to get the requested product edit page as a different user" do
+      sign_in different_user
+      get :edit, {:id => product.to_param}
+      expect(response).to redirect_to(products_path)
+    end
+
+    it "gets the product page as the product user" do
       sign_in user
-      get :edit, {:id => product.to_param}, valid_session
-      expect(assigns(:product)).to eq(product)
+      get :edit, {:id => product.to_param}
+      expect(assigns(:product)).to( eq(product) )
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
+
+      it "should fail to add a new product without authorization" do
+        sign_in nil
+        expect {
+          post :create, {:product => product_attrs }
+        }.to change(Product, :count).by(0)
+      end
+
       it "creates a new Product with authorization" do
         sign_in user
         expect {
-          post :create, {:product => product_attrs }, valid_session
+          post :create, {:product => product_attrs }
         }.to change(Product, :count).by(1)
       end
 
       it "assigns a newly created product as @product" do
-        post :create, {:product => valid_attributes}, valid_session
+        sign_in user
+        post :create, {:product => product_attrs}
         expect(assigns(:product)).to be_a(Product)
         expect(assigns(:product)).to be_persisted
       end
 
       it "redirects to the created product" do
-        post :create, {:product => valid_attributes}, valid_session
+        sign_in user
+        post :create, {:product => valid_attributes}
         expect(response).to redirect_to(Product.last)
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved product as @product" do
-        post :create, {:product => invalid_attributes}, valid_session
-        expect(assigns(:product)).to be_a_new(Product)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:product => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
   end
 
   describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+
+
+    # it "not update if user is not the author of the product" do
+    #   sign_in different_user
+    #   product.name = "Playstation 5"
+    #   put :update, {:id => product.to_param, :product => product_attrs}
+    #   expect(product.name).to_not eq("Playstation 5")
+    # end
+
+    context "when user is signed in" do
+
+      before(:each) {sign_in user}
 
       it "updates the requested product" do
-        put :update, {:id => product.to_param, :product => new_attributes}, valid_session
-        product.reload
-        skip("Add assertions for updated state")
+        product.name = "Playstation 5"
+        put :update, {:id => product.to_param, :product => product_attrs}
+        expect(product.name).to eq("Playstation 5")
       end
 
       it "assigns the requested product as @product" do
-        put :update, {:id => product.to_param, :product => valid_attributes}, valid_session
+        put :update, {:id => product.to_param, :product => product_attrs}
         expect(assigns(:product)).to eq(product)
       end
 
       it "redirects to the product" do
-        put :update, {:id => product.to_param, :product => valid_attributes}, valid_session
+        put :update, {:id => product.to_param, :product => product_attrs}
         expect(response).to redirect_to(product)
       end
+
     end
 
-    context "with invalid params" do
-      it "assigns the product as @product" do
-        put :update, {:id => product.to_param, :product => invalid_attributes}, valid_session
-        expect(assigns(:product)).to eq(product)
-      end
-
-      it "re-renders the 'edit' template" do
-        put :update, {:id => product.to_param, :product => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
   end
 
   describe "DELETE #destroy" do
+
     it "destroys the requested product" do
-      product = user.products.create! valid_attributes
+      sign_in user
+      product = user.products.create! product_attrs
       expect {
-        delete :destroy, {:id => product.to_param}, valid_session
+        delete :destroy, {:id => product.to_param}
       }.to change(Product, :count).by(-1)
     end
 
     it "redirects to the products list" do
+      sign_in user
       product = user.products.create! valid_attributes
-      delete :destroy, {:id => product.to_param}, valid_session
+      delete :destroy, {:id => product.to_param}
       expect(response).to redirect_to(products_url)
     end
+
   end
 
 end
