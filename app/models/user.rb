73f2662532f7e35ac3583c7364feefe425fcc7ee
sakoh <sakohartounian@yahoo.com>
2015-06-19 14:22:01 -0700

    validation added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :products
+  validates :first_name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :last_name, presence: true, length: {minimum: 2, maximum: 100}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,42 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "create new user" do
+  let(:user) { create(:user) }
 
-    user = create(:user)
-
-    expect(User.all).to include(user)
-
+  it "should require a first name" do
+	  user.first_name = ''
+    user.save
+    expect(user).to_not be_valid
   end
 
-  it "should have products" do
-	  user = create(:user)
-	  product = build(:product)
-	  product.user_id = user.id
-	  product.save 
-	  expect(user.products).to include(product)
+  it "should not have too short of a first name" do
+    user.first_name = 'a' 
+    user.save
+    expect(user).to_not be_valid
+  end
+
+  it "should not have too long of a first name" do
+    user.first_name = 'a' * 51 
+    user.save
+    expect(user).to_not be_valid
+  end
+
+  it "should require a last name" do
+    user.last_name = ''
+    user.save
+    expect(user).to_not be_valid
+  end
+
+  it "should not have too short of a last name" do
+    user.last_name = 'a' 
+    user.save
+    expect(user).to_not be_valid
+  end
+
+  it "should not have too long of a last name" do
+    user.last_name = 'a' * 101
+    user.save
+    expect(user).to_not be_valid
   end
 
 end
