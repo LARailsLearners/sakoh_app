c53dc1d5febd524182e57079c9f5b36b90f5c02f
sakoh <sakohartounian@yahoo.com>
2015-06-19 14:32:59 -0700

    validation added to the product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,11 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-	it "should belong to a user" do
-	  user = create(:user)
-	  product = build(:product)
-	  product.user_id = user.id
-	  product.save
-	  expect(product.user).to eq(user) 
-	end
+  let(:user) { create(:user) }
+  let(:product) { user.products.create(attributes_for :product ) }
+
+  it "should belong to a user" do
+  	product.user_id = ""
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should a name" do
+  	product.name = ""
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should not have too short of a name" do
+  	product.name = "aa"
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should not have too long of a name" do
+  	product.name = "a" * 29
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should a description" do
+  	product.description = ""
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should not have too short of a description" do
+  	product.description = "a" * 9
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
+  it "should not have too long of a description" do
+  	product.description = "a" * 501
+  	product.save
+  	expect(product).to_not be_valid
+  end
+
 end
