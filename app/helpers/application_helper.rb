978b14fa155d51db2b4377e09f7507e5e613f6d9
sakoh <sakohartounian@yahoo.com>
2015-06-23 00:07:55 -0700

    error messages have been changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(flash_key)
+    case flash_key
+    when "notice" then "alert alert-info"
+    when "success" then "alert alert-success"
+    when "error" then "alert alert-danger"
+    when "alert" then "alert alert-danger"
+    when "devise_error_messages" then "alert alert-danger"
+    else "alert"
+    end
+  end
 end
