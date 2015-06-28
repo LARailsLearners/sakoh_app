93974ff09c26bc5fcf41d712151f5c5719bf0b68
sakoh <sakohartounian@yahoo.com>
2015-06-27 21:46:02 -0700

    wrote a Reviews Controller with passing tests

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 require 'support/controller_helpers'
 
 RSpec.describe ReviewsController, type: :controller do
-  
+
   let(:user) { create(:user) }
 
   let(:product) { user.products.create(attributes_for(:product)) }
 
-  let(:review_attrs) { Hash[attributes_for(:review), user_id: user.id] }
+  let(:review_attrs) { { body: "Hello", user_id: user.id } }
 
-  let(:review) do
-  	review = product.reviews.build(review_attrs)
+  let(:review) { product.reviews.create(review_attrs) }
+
+  describe "Get #index" do
+    it "assigns the requested review as @reviews" do
+      get :index, { product_id: product.id, format: :json }
+      expect(response).to be_success
+      expect(assigns(:reviews)).to eq([review])
+    end
   end
 
-  it "assigns the requested review as @reviews" do
-    get :index, { product_id: product.id }
-    expect(response).to be_success
-    expect(assigns(:reviews)).to eq([review])
+  describe "Get #show" do
+    it "assigns the requested review as @review" do
+      get :show, { product_id: product.id, id: review.id, format: :json }
+      expect(response).to be_success
+      expect(assigns(:review)).to eq(review)
+    end
   end
 
   describe "POST #create" do
@@ -24,31 +32,15 @@ RSpec.describe ReviewsController, type: :controller do
     it "should fail to add a new review without authorization" do
       sign_in nil
       expect {
-        post :create, {:review => review }
+        post :create, {:review => review_attrs, product_id: product.id, format: :json }
       }.to change(Review, :count).by(0)
     end
 
     it "creates a new review with authorization" do
       sign_in user
       expect {
-        post :create, {:review => review_attrs }
+        post :create, {:review => review_attrs, product_id: product.id, format: :json }
       }.to change(Review, :count).by(1)
-    end
-
-    it "assigns a newly created product as @product" do
-      sign_in user
-      post :create, {:review => review_attrs}
-      expect(assigns(:review)).to be_a(Review)
-      expect(assigns(:review)).to be_persisted
-    end
-
-  end
-
-  describe "GET #show" do
-
-    it "assigns the requested review as @review" do
-      get :show, {:id => review.to_param, :product_id => product.id }
-      expect(assigns(:review)).to eq(review)
     end
 
   end
