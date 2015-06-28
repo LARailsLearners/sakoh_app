7321be6eef2e3ef578ee88eae6492ee10c41d096
sakoh <sakohartounian@yahoo.com>
2015-06-28 16:53:57 -0700

    model validation code has been reformated

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   validates :description, presence: true, length: {minimum: 10, maximum: 2000}
   validates :price, presence: true
   validates :user_id, presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/350x150"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  							:default_url => "http://placehold.it/350x150"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
-  validates :rating, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
+  validates :rating, presence: true, numericality: 
+  										{greater_than: 0, less_than_or_equal_to: 5}
   validates :body, presence: true
   validates :user_id, presence: true, uniqueness: true
   validates :product_id, presence: true
